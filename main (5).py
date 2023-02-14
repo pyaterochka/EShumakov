3d5bef6985651b559117074d6e2f6716e919264f
pyaterochka <125362747+pyaterochka@users.noreply.github.com>
2023-02-14 14:34:49 +0300

    Add files via upload

---
+++ main (5).py
@@ -0,0 +1,15 @@
+tseloe = 10
+drobnoe = 8.4
+stroka = 'No'
+stroka2 = 'Yes'
+stroka3 = '_'
+big_netseloe = tseloe * 3.5
+drobnoe -= 1
+stroka = stroka * 2
+stroka = stroka * 2 + "_" + "Yes" * 3
+print (tseloe / drobnoe)
+print (big_netseloe / drobnoe)
+print (tseloe)
+print (drobnoe)
+print (stroka)
+print (big_netseloe)
---
+++ main (6).py
@@ -0,0 +1,17 @@
+g = 1
+d = 2
+print ((g>d) and (d>g))
+print ((d>g) and (g<d))
+print ((g>d) or (d>g))
+print (not (d>g))
+c = g + d
+print ((c>d) or (d>g))
+print ((g>c) or (d>c))
+print ((c<d) or (d<g))
+print ((g<c) or (d>g))
+f = 'csk'
+h = 'fbr'
+print ((f>h) and (h>f))
+print ((h>f) or (h>f))
+print ((c>d) or (d>g))
+print (not (f<h))
