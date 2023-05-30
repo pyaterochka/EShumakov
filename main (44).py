389acbeb21177070024749e37cac0e40276a3358
pyaterochka <125362747+pyaterochka@users.noreply.github.com>
2023-05-30 16:02:33 +0300

    год

---
+++ main (44).py
@@ -0,0 +1,10 @@
+def odin(a,b):
+    uzbek = 0
+    if a>b:
+        uzbek = a 
+        print(uzbek)
+    elif b>a:
+        uzbek = b 
+        print(uzbek)
+if __name__ == '__main__':
+    odin(a=2, b=3)
