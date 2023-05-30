c894836c926f137d1352fdbc08db9077cb4f790d
pyaterochka <125362747+pyaterochka@users.noreply.github.com>
2023-05-30 15:54:54 +0300

    Классная работа 30.052023

---
+++ main (45).py
@@ -0,0 +1,9 @@
+def odin(a,b):
+    payaterochka = 0
+    if a>b:
+        payaterochka = a 
+        print(payaterochka)
+    elif b>a:
+        payaterochka = b 
+        print(payaterochka)
+odin(a=2, b=3)
